a1553450ced41eb9fc566c216e9c115e9d2ab4d1
dreamelax <Sibisatheeshk@gmail.com>
2024-02-09 19:23:15 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Transform transform;
+    public Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        rigidbody2D.AddForce(new Vector2(0.5f,1));
+    }
+}
