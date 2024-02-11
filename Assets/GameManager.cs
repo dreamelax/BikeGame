de199d39254a68284df772da224b9bbd20720a8b
LaytonMadeira <layton.madeira@stonybrook.edu>
2024-02-10 23:48:58 -0500

    doodad
    We have set the ground for remarkable and world-changing, life-saving, mind-bending programming epiphanies.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,13 +9,22 @@ public class GameManager : MonoBehaviour
     private int playerNum;
     // ["A","D","W"] = 1 player with A to turne left, D to turn right, W to boost
     // ["A","D","W","left","right","up"] = 2 players, player 1 has A to left, D to right, W to boost. player 2 has ye self-explainitory
-    private string[] playerkeybinds; 
+    private static string[] playerkeybinds; 
+    private List <GameObject> players = new List<GameObject>();
+    public GameObject spawn1;
+    public GameObject spawn2;
+    public GameObject spawn3;
+    public GameObject spawn4;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerNum = 2;
+        playerkeybinds = new string[6];
+        playerkeybinds[0] = "a"; playerkeybinds[1] = "d"; playerkeybinds[2] = "w";
+        playerkeybinds[3] = "left"; playerkeybinds[4] = "right"; playerkeybinds[5] = "up";
+        resetGame();
     }
 
     // Update is called once per frame
@@ -26,9 +35,49 @@ public class GameManager : MonoBehaviour
 
     // RESET GAME
     public void resetGame(){
+        if(playerNum == 2){
+            GameObject p1 = Instantiate (Resources.Load("player", typeof(GameObject))) as GameObject;
+            GameObject p2 = Instantiate (Resources.Load("player", typeof(GameObject))) as GameObject;
+            players.Add(p1);
+            players.Add(p2);
+            p1.GetComponent<PlayerMovement>().setKeyBinds(playerkeybinds[0], playerkeybinds[1], playerkeybinds[2]);
+            p2.GetComponent<PlayerMovement>().setKeyBinds(playerkeybinds[3], playerkeybinds[4], playerkeybinds[5]);
+            p1.transform.position = spawn1.transform.position;
+            p2.transform.position = spawn2.transform.position;
+            Vector3 vRot = new Vector3(0,0,180);
+            p1.transform.Rotate(vRot, Space.Self);
+        }
+        else if(playerNum == 3){
+            GameObject p1 = Instantiate (Resources.Load("player", typeof(GameObject))) as GameObject;
+            GameObject p2 = Instantiate (Resources.Load("player", typeof(GameObject))) as GameObject;
+            GameObject p3 = Instantiate (Resources.Load("player", typeof(GameObject))) as GameObject;
+            players.Add(p1);
+            players.Add(p2);
+            players.Add(p3);
+            p1.GetComponent<PlayerMovement>().setKeyBinds(playerkeybinds[0], playerkeybinds[1], playerkeybinds[2]);
+            p1.GetComponent<PlayerMovement>().setKeyBinds(playerkeybinds[3], playerkeybinds[4], playerkeybinds[5]);
+            p1.GetComponent<PlayerMovement>().setKeyBinds(playerkeybinds[7], playerkeybinds[7], playerkeybinds[8]);
+        }   
+        else if(playerNum == 4){
+            GameObject p1 = Instantiate (Resources.Load("player", typeof(GameObject))) as GameObject;
+            GameObject p2 = Instantiate (Resources.Load("player", typeof(GameObject))) as GameObject;
+            GameObject p3 = Instantiate (Resources.Load("player", typeof(GameObject))) as GameObject;
+            GameObject p4 = Instantiate (Resources.Load("player", typeof(GameObject))) as GameObject;   
+            players.Add(p1);
+            players.Add(p2);
+            players.Add(p3);
+            players.Add(p4);
+            p1.GetComponent<PlayerMovement>().setKeyBinds(playerkeybinds[0], playerkeybinds[1], playerkeybinds[2]);
+            p1.GetComponent<PlayerMovement>().setKeyBinds(playerkeybinds[3], playerkeybinds[4], playerkeybinds[5]);
+            p1.GetComponent<PlayerMovement>().setKeyBinds(playerkeybinds[7], playerkeybinds[7], playerkeybinds[8]);
+            p1.GetComponent<PlayerMovement>().setKeyBinds(playerkeybinds[9], playerkeybinds[10], playerkeybinds[11]);            
 
+        }   
     }
 
+    public string [] getKeyBinds(){
+        return playerkeybinds;
+    }
 
 
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,17 +11,74 @@ public class PlayerMovement : MonoBehaviour
     public GameManager gamemanager;
 
     public float runspeed;
-    
+    public float horizontal;
+    public float vertical;
+    public float inversionFactor;
+    public Vector2 movementvector;
 
+    string [] inps;
     // Start is called before the first frame update
     void Start()
     {
         
+        // intial movement 
+        horizontal = -1; 
+        vertical   = 0;
+        inversionFactor = 1;
+        movementvector = new Vector2(horizontal, vertical);
+        runspeed = 20f;
+        
     }
 
+    void Awake(){
+        inps = new string [3];
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
+        // float horizontal = Input.GetAxisRaw("Horizontal"); 
+        // float vertical   = Input.GetAxisRaw("Vertical");
+        // Vector2 movementvector = new Vector2(horizontal, vertical);
+        // movementVector.Normalize(); //-> keeps diagonal movement at the same speed
+        // transform.Translate(movementvector*runspeed*Time.deltaTime);  
+
+        // getting keybinds from gamemanager , order: left - right - boost 
+        // Debug.Log("hello");
+        // Debug.Log(inps[0]);
+
+        if(Input.GetKeyDown(""+inps[0])){
+            Debug.Log("NOT Hooray~!");
+            horizontal = -1;
+            vertical   = 0;
+            inversionFactor = 1;
+            // transform.localRotation = Quaternion.Euler(0, 0, 0);
+            Vector3 vRot = new Vector3(0,0,90);
+            transform.Rotate(vRot, Space.Self);
+        }
+
+
+        if(Input.GetKeyDown(""+inps[1])){
+            Debug.Log("Hooray~!");
+            horizontal = 1; 
+            vertical   = 0;
+            inversionFactor = -1;
+            // Quaternion q1 = Quaternion(90,0,0);
+            // transform.LookAt(new Vector2(transform.localPosition.x+1,transform.localPosition.y+1));
+            // transform.localRotation = Quaternion.Euler(0, 0, 180);
+            Vector3 vRot = new Vector3(0,0,270);
+            transform.Rotate(vRot, Space.Self);
+        }
+
+
+        movementvector = new Vector2(horizontal*inversionFactor, vertical);
+        transform.Translate(movementvector*runspeed*Time.deltaTime);  
+
+    }
+
+    public void setKeyBinds(string s1, string s2, string s3){
+        inps[0] = s1;
+        inps[1] = s2;
+        inps[2] = s3;
     }
 }
